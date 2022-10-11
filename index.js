c6f79534058d08f6e7e9b662f943f2b4f2c5969f
rizalarif <rizal.arif2001@gmail.com>
2022-10-11 21:58:31 +0700

    ...

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 //Import Readline Module for Command Line
+const { rejects } = require('assert');
+const { resolve } = require('path');
 const rLine =  require('readline');
 const cmd = rLine.createInterface(process.stdin,process.stdout)
 
 // Program Hitung Ongkos Kirim
-    console.log("===    Selamat Datang    ===")
-    console.log("= Hitung Ongkos Kirim Anda =")
-    console.log("============================")
 
-    cmd.question(`Berapa Jarak Tempuh Anda : `,(input)=>{
-        if(input <= 2){
-            uang = 8000;
-            console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(uang))
-            layanan = 0.045 * uang
-            console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(layanan))
-            cmd.close();
-        }
-        else if(input => 3){
-            uang = 8000 + (5000*(input-2))
-            console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(uang))
-            layanan = 0.045 * uang
-            console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(layanan))
-            cmd.close();
+const error = () =>{
+    cmd.close()
+}
+
+
+const mulai = async() => {
+    let running = true;
+
+    while(running){
+        try{
+        console.log("===    Selamat Datang    ===")
+        console.log("= Hitung Ongkos Kirim Anda =")
+        console.log("============================")
+
+        cmd.question(`Berapa Jarak Tempuh Anda : `,(input)=>{
+            if(input <= 2){
+                uang = 8000;
+                console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(uang))
+                layanan = 0.045 * uang
+                console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(layanan))
             }
-        })
-// kurang while
+            else if(input => 3){
+                uang = 8000 + (5000*(input-2))
+                console.log("Total Ongkos Kirim : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(uang))
+                layanan = 0.045 * uang
+                console.log("Biaya Layanan : " + new Intl.NumberFormat("id-ID", { style: "currency", currency: "IDR" }).format(layanan))
+                }
+            })
+        } catch(error){
+            console.log("Terimakasih!");
+            running = false
+        }
+    }
+}
+
+mulai();
+
+
+
+
+
